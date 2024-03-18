a9f3b0c71a8b41aafb46b76abb449d6b33eb1698
Ron Felsenfeld <ronfelsenfeld@gmail.com>
2024-03-18 16:44:16 +0200

    Add api/bug - read bugs

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import express from 'express'
 
 import { bugService } from './services/bug.service.js'
+import { loggerService } from './services/logger.service.js'
 
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello there!'))
 
 app.get('/api/bug', (req, res) => {
-  res.send('/api/bug!')
+  bugService
+    .query()
+    .then(bugs => res.send(bugs))
+    .catch(err => {
+      loggerService.error('Cannot get bugs:', err)
+      res.status(400).send('Cannot get bugs')
+    })
 })
 
 app.get('/api/bug/save', (req, res) => {
